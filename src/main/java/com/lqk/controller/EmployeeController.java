65df2f0a597d3fb8abb19f20d15d42e4821bfd8e
qkliang <qkliang@foxmail.com>
2020-04-24 18:13:22 +0800

    20200424

--- src/main/java/com/lqk/controller/EmployeeController.java
+++ src/main/java/com/lqk/controller/EmployeeController.java
@@ -1,10 +1,14 @@
 package com.lqk.controller;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.lqk.bean.Employee;
 import com.lqk.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
@@ -14,9 +18,14 @@ public class EmployeeController {
     @Autowired
     EmployeeService employeeService;
     @RequestMapping("/emps")
-    public String getEmployees(){
+    public String getEmployees(@RequestParam(value = "pn",defaultValue = "1") Integer pn,
+    ModelAndView m){
+        PageHelper.startPage(pn,5);
         List<Employee> employees = employeeService.getAll();
         System.out.println(employees.size());
+        PageInfo pageInfo = new PageInfo(employees, 5);
+        m.addObject("pageInfo",pageInfo);
+        System.out.println(pageInfo);
         return "list";
     }
 }
