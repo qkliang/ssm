204409808c90644e06070fae3521860ec57612d1
qkliang <qkliang@foxmail.com>
2020-05-11 10:52:35 +0800

    提交

---
+++ src/main/java/com/lqk/juc/VolatileDemo.java
@@ -0,0 +1,33 @@
+package com.lqk.juc;
+public class VolatileDemo {
+    public static void main(String[] args) {
+        MyThread myThread = new MyThread();
+        new Thread(myThread).start();
+        while (true){
+            if(myThread.isFlag()){
+                System.out.println("-----------------");
+                break;
+            }
+        }
+    }
+}
+class MyThread extends Thread{
+    public volatile boolean flag = false;
+    @Override
+    public void run() {
+        try {
+            Thread.sleep(200);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        flag = true;
+        System.out.println("flag = " + isFlag());
+    }
+    public boolean isFlag() {
+        return flag;
+    }
+
+    public void setFlag(boolean flag) {
+        this.flag = flag;
+    }
+}
